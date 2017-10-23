03fb19bf481d24e6be6eea86b99d4273ec994a24
ramon.singh@blackcrowsys.com <ramon@MacBook-Air.local>
2017-10-23 14:42:42 +0100

    Project Initialized

---
+++ src/main/java/biz/dinewell/dinewellcarehome/DinewellCarehomeApplication.java
@@ -0,0 +1,12 @@
+package biz.dinewell.dinewellcarehome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinewellCarehomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DinewellCarehomeApplication.class, args);
+    }
+}
